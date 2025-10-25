37d02fd907b54a5f4459b5d1ecd66d6fdf8c967e
Die <dlibonati@leafnoise.io>
2025-10-25 00:14:08 -0300

    feat: better response api services

--- src/api/get/getTours.ts
+++ src/api/get/getTours.ts
@@ -10,15 +10,7 @@ export const getTours = async (): Promise<GetToursResponse> => {
 
     const data: GetToursResponse = await response.json();
 
-    const tours = data.map((tour) => ({
-      id: tour.id,
-      name: tour.name,
-      info: tour.info,
-      image: tour.image,
-      price: tour.price,
-    }));
-
-    return tours;
+    return data;
   } catch (e) {
     throw new Error(`Error fetching tours: ${e}.`);
   }
